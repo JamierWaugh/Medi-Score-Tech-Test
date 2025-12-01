bc6de28867c99b8f9835043e0817f8a516a960c4
Jamie Waugh <c4029539@newcastle.ac.uk.com>
2025-12-01 14:20:37 +0000

    Created initial PatientData class setup PatientData holds all of the current attributes with ability to easily scale more attributes in. I plan on implementing scoring functions next

--- README.md
+++ README.md
@@ -1,2 +1,6 @@
 # Medi-Score-Tech-Test
 Medi score tech test for airelogic
+## Patient Data
+I have decided to create a class to facilitate the storage of the health data attributes as Patient Data. This will help ensure scalability in the solution and data consistency across functions. To implement this safely, I have enforced strict type control on initialisation so the system can fail fast with incorrect values. 
+
+Furthermore, I have created Enum classes to facilitate setting of unique values such as AIR for on_oxygen values. This comes in handy when handling data that is not as simple as an integer value
---
+++ patient_data.py
@@ -0,0 +1,37 @@
+from enum import IntEnum
+"""
+Patient data will hold the PatientData class definition and functions
+Each patients data will be set using this class, ensuring scalable and safe operations for the MediScore
+"""
+
+#Enum  object for on_oxygen value
+class OnOxygen(IntEnum):
+    AIR = 0
+    OXYGEN = 2
+
+#Enum object for consciousness value
+class Consciousness(IntEnum):
+    ALERT = 0
+    CVPU = 1
+
+class PatientData():
+    def __init__(self, on_oxygen, consciousness, resp_rate, oxygen_sat,  temp ):
+        
+        #Enforce types of each value
+        if not isinstance(on_oxygen, OnOxygen):
+            raise TypeError("Attribute on_oxygen is not of type OnOxygen")
+        if not isinstance(consciousness, Consciousness):
+            raise TypeError("Attribute consciousness is not of type Consciousness")
+        if not isinstance(resp_rate, int):
+            raise TypeError("Attirbute resp_rate is not of type int")
+        if not isinstance(oxygen_sat, int):
+            raise TypeError("Attribute oxygen_sat is not of type int")
+        if not isinstance(temp, float):
+            raise TypeError("Attribute temp is not of type float")
+        self.on_oxygen = on_oxygen
+        self.resprate = resp_rate
+        self.oxygen_sat = oxygen_sat
+        self.consciousness = consciousness
+        self.temp = round(temp, 1) #Round to one decimal point
+
+
